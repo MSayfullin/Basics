b283514b79fc44a1a32bf710f7d5a3dab34f08e4
Marsel Sayfullin <M.Sayfullin@gmail.com>
2013-10-25 07:10:43 +0400

    Implement Binary Search

---
+++ Basics.Algorithms.Tests/BinarySearchTests.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using Basics.Algorithms;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace Basics.Algorithms.Tests
+{
+    [TestClass]
+    public class BinarySearchTests
+    {
+        [TestMethod]
+        public void BinarySearch_NoKeyFound()
+        {
+            var array = new int[] { 0, 1, 2, 3, 4, 5, 6, 7, 8, 9 };
+            var index  = array.IndexOf(10);
+            Assert.AreEqual(-1, index);
+        }
+
+        [TestMethod]
+        public void BinarySearch_KeyExists()
+        {
+            var array = new int[] { 0, 1, 2, 3, 4, 5, 6, 7, 8, 9 };
+            var index = array.IndexOf(3);
+            Assert.AreEqual(3, index);
+        }
+
+        [TestMethod]
+        public void BinarySearch_FirstKey()
+        {
+            var array = new int[] { 0, 1, 2, 3, 4, 5, 6, 7, 8, 9 };
+            var index = array.IndexOf(0);
+            Assert.AreEqual(0, index);
+        }
+
+        [TestMethod]
+        public void BinarySearch_LastKey()
+        {
+            var array = new int[] { 0, 1, 2, 3, 4, 5, 6, 7, 8, 9 };
+            var index = array.IndexOf(9);
+            Assert.AreEqual(9, index);
+        }
+
+        [TestMethod]
+        public void BinarySearch_MidddleKey()
+        {
+            var array = new int[] { 0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10 };
+            var index = array.IndexOf(5);
+            Assert.AreEqual(5, index);
+        }
+    }
+}
---
+++ Basics.Algorithms/BinarySearch.cs
@@ -0,0 +1,56 @@
+﻿using System;
+
+namespace Basics.Algorithms
+{
+    public static class BinarySearch
+    {
+        /// <summary>
+        /// Returns index of a key in an array using binary search algorithm.
+        /// </summary>
+        /// <typeparam name="T">
+        /// Type of elements in an array.
+        /// Type should implement <see cref="IComparable{T}"/> or <see cref="IComparable"/>
+        /// </typeparam>
+        /// <param name="source">Source array.</param>
+        /// <param name="key">Key to search in an array.</param>
+        /// <returns></returns>
+        public static int IndexOf<T>(this T[] source, T key) where T : IComparable<T>, IComparable
+        {
+            return EthalonImplementation<T>(source, key);
+            //return RecursiveImplementation<T>(source, key);
+        }
+
+        private static int EthalonImplementation<T>(T[] source, T key) where T : IComparable<T>, IComparable
+        {
+            int lo = 0;
+            int hi = source.Length - 1;
+            while (lo <= hi)
+            {
+                int middle = (hi - lo) / 2 + lo;
+                int res = key.CompareTo(source[middle]);
+                if (res < 0) hi = middle - 1;
+                else if (res > 0) lo = middle + 1;
+                else return middle;
+            }
+            return -1;
+        }
+
+        private static int RecursiveImplementation<T>(T[] source, T key) where T : IComparable<T>, IComparable
+        {
+            return RecursiveImplementation(source, key, lo: 0, hi: source.Length - 1);
+        }
+
+        private static int RecursiveImplementation<T>(T[] source, T key, int lo, int hi) where T : IComparable<T>, IComparable
+        {
+            if (lo > hi) return -1;
+
+            int middle = (hi - lo) / 2 + lo;
+            int res = key.CompareTo(source[middle]);
+            if (res < 0)
+                return RecursiveImplementation(source, key, lo: lo, hi: middle - 1);
+            else if (res > 0)
+                return RecursiveImplementation(source, key, lo: middle + 1, hi: hi);
+            else return middle;
+        }
+    }
+}
