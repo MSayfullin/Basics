0d9331696cbc22d4d3406c51626812dbcc6956ec
Marsel Sayfullin <M.Sayfullin@gmail.com>
2013-10-26 22:14:51 +0400

    Add IQueue interface

---
+++ Basics.Structures/Queues/IQueue.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace Basics.Structures
+{
+    public interface IQueue<T> : IEnumerable<T>
+    {
+        /// <summary>
+        /// Enqueues value to the queue.
+        /// </summary>
+        void Enqueue(T value);
+
+        /// <summary>
+        /// Dequeues value out of the queue.
+        /// </summary>
+        /// <exception cref="InvalidOperationException">Thrown when queue is empty.</exception>
+        T Dequeue();
+
+        /// <summary>
+        /// Checks whether queue is empty.
+        /// </summary>
+        bool IsEmpty { get; }
+
+        /// <summary>
+        /// Returns size of the queue.
+        /// </summary>
+        int Size { get; }
+    }
+}
