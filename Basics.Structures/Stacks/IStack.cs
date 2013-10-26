36f97b93440ccba7d995d1530f9de7ebd50ca7e5
Marsel Sayfullin <M.Sayfullin@gmail.com>
2013-10-26 12:59:22 +0400

    Add IStack interface

---
+++ Basics.Structures/Stacks/IStack.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace Basics.Structures
+{
+    public interface IStack<T> : IEnumerable<T>
+    {
+        /// <summary>
+        /// Pushes value to the stack.
+        /// </summary>
+        void Push(T value);
+
+        /// <summary>
+        /// Pops value out of the stack.
+        /// </summary>
+        /// <exception cref="InvalidOperationException">Thrown when stack is empty.</exception>
+        T Pop();
+
+        /// <summary>
+        /// Checks whether stack is empty.
+        /// </summary>
+        bool IsEmpty { get; }
+
+        /// <summary>
+        /// Returns size of the stack.
+        /// </summary>
+        int Size { get; }
+    }
+}
