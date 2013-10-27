d8ccff9ea733bf94094507c29e7589ac28224fbe
Marsel Sayfullin <M.Sayfullin@gmail.com>
2013-10-27 08:01:27 +0400

    Implement Queue on Resizing Array

---
+++ Basics.Structures.Tests/QueueTests.cs
@@ -0,0 +1,187 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace Basics.Structures.Tests
+{
+    [TestClass]
+    public class QueueTests
+    {
+        #region Queue on Array
+
+        [TestMethod]
+        public void QueueOnArray_Empty_IEnumeratorTest()
+        {
+            var queue = new QueueOnArray<int>();
+            CheckEmptyEnumerator(queue);
+        }
+
+        [TestMethod]
+        public void QueueOnArray_Full_IEnumeratorTest()
+        {
+            var queue = new QueueOnArray<int>();
+            CheckFullEnumerator(queue);
+        }
+
+        [TestMethod]
+        public void QueueOnArray_Full_AfterDequeue_IEnumeratorTest()
+        {
+            var queue = new QueueOnArray<int>();
+            CheckFullEnumeratorAfterDequeue(queue);
+        }
+
+        [TestMethod]
+        public void QueueOnArray_Double_IEnumeratorTest()
+        {
+            var queue = new QueueOnArray<int>();
+            CheckDoubleEnumerator(queue);
+        }
+
+        [TestMethod]
+        public void QueueOnArray_EnqueueTest()
+        {
+            var queue = new QueueOnArray<string>();
+            CheckEnqueue(queue);
+        }
+
+        [TestMethod]
+        [ExpectedException(typeof(InvalidOperationException))]
+        public void QueueOnArray_DequeueFromEmptyQueueTest()
+        {
+            var queue = new QueueOnArray<string>();
+            queue.Dequeue();
+        }
+
+        [TestMethod]
+        public void QueueOnArray_DequeueTest()
+        {
+            var queue = new QueueOnArray<string>();
+            CheckDequeue(queue);
+        }
+
+        [TestMethod]
+        public void QueueOnArray_MultipleDequeueTest()
+        {
+            var queue = new QueueOnArray<string>();
+            CheckMultipleDequeue(queue);
+        }
+
+        #endregion
+
+        #region Checks
+
+        private void CheckEmptyEnumerator<T>(IQueue<T> queue)
+        {
+            foreach (var item in queue)
+            {
+                Assert.Fail("Check enumerator implementation. Queue is empty.");
+            }
+        }
+
+        private void CheckFullEnumerator(IQueue<int> queue)
+        {
+            for (int i = 0; i < 10; i++)
+            {
+                queue.Enqueue(i);
+            }
+            int counter = 0;
+            foreach (var item in queue)
+            {
+                Assert.AreEqual(counter++, item);
+            }
+        }
+
+        private void CheckFullEnumeratorAfterDequeue(IQueue<int> queue)
+        {
+            for (int i = 0; i < 10; i++)
+            {
+                queue.Enqueue(i);
+            }
+            queue.Dequeue();
+            queue.Dequeue();
+            int counter = 2;
+            foreach (var item in queue)
+            {
+                Assert.AreEqual(counter++, item);
+            }
+        }
+
+        private void CheckDoubleEnumerator(IQueue<int> queue)
+        {
+            int counter1 = 0;
+            for (int i = 0; i < 10; i++)
+            {
+                queue.Enqueue(i);
+            }
+            int counter2 = 0;
+            foreach (var item1 in queue)
+            {
+                Assert.AreEqual(counter1++, item1);
+                foreach (var item2 in queue)
+                {
+                    Assert.AreEqual(counter2++, item2);
+                }
+                counter2 = 0;
+            }
+        }
+
+        private void CheckEnqueue(IQueue<string> queue)
+        {
+            Assert.IsTrue(queue.IsEmpty);
+            Assert.AreEqual(0, queue.Size);
+
+            queue.Enqueue("test");
+
+            Assert.IsFalse(queue.IsEmpty);
+            Assert.AreEqual(1, queue.Size);
+        }
+
+        private void CheckDequeue(IQueue<string> queue)
+        {
+            var testData = "test";
+            queue.Enqueue(testData);
+
+            var dequeuedData = queue.Dequeue();
+
+            Assert.IsTrue(queue.IsEmpty);
+            Assert.AreEqual(0, queue.Size);
+            Assert.AreEqual(testData, dequeuedData);
+        }
+
+        private void CheckMultipleDequeue(IQueue<string> queue)
+        {
+            var testData = "test";
+            queue.Enqueue(testData);
+            queue.Enqueue("otherData");
+            queue.Enqueue("anotherData");
+            queue.Enqueue("moreData");
+            queue.Enqueue("evenMoreData");
+
+            var dequeuedData = queue.Dequeue();
+            Assert.IsFalse(queue.IsEmpty);
+            Assert.AreEqual(4, queue.Size);
+            Assert.AreEqual(testData, dequeuedData);
+
+            dequeuedData = queue.Dequeue();
+            Assert.IsFalse(queue.IsEmpty);
+            Assert.AreEqual(3, queue.Size);
+            Assert.AreNotEqual(testData, dequeuedData);
+
+            dequeuedData = queue.Dequeue();
+            Assert.IsFalse(queue.IsEmpty);
+            Assert.AreEqual(2, queue.Size);
+            Assert.AreNotEqual(testData, dequeuedData);
+
+            dequeuedData = queue.Dequeue();
+            Assert.IsFalse(queue.IsEmpty);
+            Assert.AreEqual(1, queue.Size);
+            Assert.AreNotEqual(testData, dequeuedData);
+
+            dequeuedData = queue.Dequeue();
+            Assert.IsTrue(queue.IsEmpty);
+            Assert.AreEqual(0, queue.Size);
+            Assert.AreNotEqual(testData, dequeuedData);
+        }
+
+        #endregion
+    }
+}
---
+++ Basics.Structures/Queues/QueueOnArray.cs
@@ -0,0 +1,158 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+
+namespace Basics.Structures
+{
+    public class QueueOnArray<T> : IQueue<T>
+    {
+        private const int initialSize = 4;
+
+        private int headIndex = 0;
+        private int tailIndex = 0;
+        private T[] _elements = new T[initialSize];
+
+        /// <summary>
+        /// Enqueues value to the queue.
+        /// </summary>
+        public void Enqueue(T value)
+        {
+            _elements[tailIndex++] = value;
+
+            if (tailIndex == _elements.Length)
+                Resize(_elements.Length * 2);
+        }
+
+        /// <summary>
+        /// Dequeues value out of the queue.
+        /// </summary>
+        /// <exception cref="InvalidOperationException">Thrown when queue is empty.</exception>
+        public T Dequeue()
+        {
+            if (tailIndex == 0)
+                throw new InvalidOperationException("Queue is empty.");
+
+            var element = _elements[headIndex];
+            // clean up resources
+            _elements[headIndex++] = default(T);
+
+            if (_elements.Length > initialSize && Size == _elements.Length / 4)
+            {
+                Resize(_elements.Length / 2);
+                tailIndex = tailIndex - headIndex;
+                headIndex = 0;
+            }
+
+            return element;
+        }
+
+        /// <summary>
+        /// Checks whether queue is empty.
+        /// </summary>
+        public bool IsEmpty
+        {
+            get { return Size == 0; }
+        }
+
+        /// <summary>
+        /// Returns size of the queue.
+        /// </summary>
+        public int Size
+        {
+            get { return tailIndex - headIndex; }
+        }
+
+        private void Resize(int newSize)
+        {
+            var newArray = new T[newSize];
+            for (int i = headIndex; i < tailIndex; i++)
+            {
+                newArray[i - headIndex] = _elements[i];
+            }
+            _elements = newArray;
+        }
+
+        #region IEnumerable Members
+
+        public IEnumerator<T> GetEnumerator()
+        {
+            return new ArrayEnumerator<T>(_elements, headIndex, tailIndex);
+            //return YieldEnumerator;
+        }
+
+        IEnumerator IEnumerable.GetEnumerator()
+        {
+            return GetEnumerator();
+        }
+
+        #endregion
+
+        #region Enumerators
+
+        private IEnumerator<T> YieldEnumerator
+        {
+            get
+            {
+                int enumerationIndex = headIndex;
+                int finish = tailIndex;
+                while (enumerationIndex < finish)
+                    yield return _elements[enumerationIndex++];
+            }
+        }
+
+        private struct ArrayEnumerator<X> : IEnumerator<X>
+        {
+            private int index;
+            private int start;
+            private int finish;
+            private X[] _elements;
+
+            public ArrayEnumerator(X[] elements, int headIndex, int tailIndex)
+            {
+                start = headIndex;
+                finish = tailIndex;
+                this.index = -1;
+                _elements = elements;
+            }
+
+            public X Current
+            {
+                get
+                {
+                    if (index == -1)
+                        throw new InvalidOperationException("Current is undefined. Call MoveNext first.");
+
+                    return _elements[index];
+                }
+            }
+
+            public bool MoveNext()
+            {
+                index = index == -1 ? start : index + 1;
+
+                return index < finish;
+            }
+
+            public void Reset()
+            {
+                index = -1;
+            }
+
+            object IEnumerator.Current
+            {
+                get { return Current; }
+            }
+
+            #region IDisposable Members
+
+            public void Dispose()
+            {
+                // nothing to dispose
+            }
+
+            #endregion
+        }
+
+        #endregion
+    }
+}
