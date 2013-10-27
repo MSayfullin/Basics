0cdb9f89ab605edc6185ebf5d9a0f7278ab4e4ea
Marsel Sayfullin <M.Sayfullin@gmail.com>
2013-10-27 15:58:58 +0400

    Draftly implement sorting performance test

---
+++ Basics.Algorithms.Tests/SortingPerformanceTests.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Diagnostics;
+using Basics.Algorithms.Sorts;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace Basics.Algorithms.Tests
+{
+    [TestClass]
+    public class SortingPerformanceTests
+    {
+        private const int size = 10000;
+
+        private class SortInfo
+        {
+            public string Name { get; set; }
+            public Action<int[]> Act { get; set; }
+        }
+
+        [TestMethod]
+        public void SortingPerformanceTest()
+        {
+            var stopwatch = new Stopwatch();
+            var sortedArray = new int[size];
+            for (int i = 0; i < size; i++)
+            {
+                sortedArray[i] = i;
+            }
+            var sorts = new SortInfo[]
+            {
+                new SortInfo { Name = "Selection Sort", Act = array => Selection.Sort(array) },
+                new SortInfo { Name = "Insertion Sort", Act = array => Insertion.Sort(array) },
+                new SortInfo { Name = "Shell Sort", Act = array => Shell.Sort(array) },
+            };
+
+            foreach (var sort in sorts)
+            {
+                //var unsortedArray = sortedArray.Shuffle();
+                stopwatch.Restart();
+                sort.Act(sortedArray);
+                stopwatch.Stop();
+                Console.WriteLine("{0}: {1}", sort.Name, stopwatch.ElapsedTicks);
+            }
+        }
+
+    }
+}
