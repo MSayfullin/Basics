7ecd3a4049c6b51f9f19ee7ad9ce56571d10600b
Marsel Sayfullin <M.Sayfullin@gmail.com>
2013-10-27 15:06:26 +0400

    Implement Shell Sort

---
+++ Basics.Algorithms.Tests/ShellSortTests.cs
@@ -0,0 +1,73 @@
+﻿using Basics.Algorithms.Sorts;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace Basics.Algorithms.Tests
+{
+    [TestClass]
+    public class ShellSortTests
+    {
+        [TestMethod]
+        public void ShellSort_Int_TestOnEmptyArray()
+        {
+            var array = new int[] { };
+            Shell.Sort(array);
+            Assert.IsTrue(array.IsSorted());
+        }
+
+        [TestMethod]
+        public void ShellSort_Int_TestOnArrayWithOneElement()
+        {
+            var array = new int[] { 1 };
+            Shell.Sort(array);
+            Assert.IsTrue(array.IsSorted());
+        }
+
+        [TestMethod]
+        public void ShellSort_Int_TestOnSortedArray()
+        {
+            var array = new int[] { 0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15 };
+            Shell.Sort(array);
+            Assert.IsTrue(array.IsSorted());
+        }
+
+        [TestMethod]
+        public void ShellSort_Int_TestOnUnsortedArray()
+        {
+            var array = new int[] { 12, 0, 3, 9, 1, 10, 4, 15, 5, 2, 13, 7, 8, 6, 11, 14 };
+            Shell.Sort(array);
+            Assert.IsTrue(array.IsSorted());
+        }
+
+        [TestMethod]
+        public void ShellSort_Int_TestOnUnsortedArrayWithDuplicates()
+        {
+            var array = new int[] { 12, 0, 3, 9, 1, 3, 4, 15, 5, 2, 13, 7, 8, 12, 11, 14, 15, 15 };
+            Shell.Sort(array);
+            Assert.IsTrue(array.IsSorted());
+        }
+
+        [TestMethod]
+        public void ShellSort_String_TestOnSortedArray()
+        {
+            var array = new string[] { "a", "b", "c", "d", "e", "f", "g" };
+            Shell.Sort(array);
+            Assert.IsTrue(array.IsSorted());
+        }
+
+        [TestMethod]
+        public void ShellSort_String_TestOnUnsortedArray()
+        {
+            var array = new string[] { "f", "b", "g", "d", "e", "a", "c" };
+            Shell.Sort(array);
+            Assert.IsTrue(array.IsSorted());
+        }
+
+        [TestMethod]
+        public void ShellSort_String_TestOnUnsortedArrayWithDuplicates()
+        {
+            var array = new string[] { "f", "b", "g", "a", "d", "e", "a", "c", "b" };
+            Shell.Sort(array);
+            Assert.IsTrue(array.IsSorted());
+        }
+    }
+}
---
+++ Basics.Algorithms/Sorts/Shell.cs
@@ -0,0 +1,27 @@
+﻿using System;
+
+namespace Basics.Algorithms.Sorts
+{
+    public static class Shell
+    {
+        public static void Sort<T>(T[] array) where T : IComparable<T>
+        {
+            int increment = 1;
+            while (increment < array.Length / 3)
+                increment = 3 * increment + 1;
+
+            while (increment >= 1)
+            {
+                for (int i = increment; i < array.Length; i++)
+                {
+                    for (int j = i; j >= increment; j -= increment)
+                    {
+                        if (array[j].IsLessThan(array[j - increment]))
+                            array.Exchange(j, j - increment);
+                    }
+                }
+                increment = increment / 3;
+            }
+        }
+    }
+}
