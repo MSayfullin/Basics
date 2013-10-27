ca66f7e469a6cd5a86a65d977ab0c06c234baed7
Marsel Sayfullin <M.Sayfullin@gmail.com>
2013-10-27 11:51:58 +0400

    Implement extensions for sorting array

---
+++ Basics.Algorithms.Tests/ArrayExtensionsTests.cs
@@ -0,0 +1,93 @@
+﻿using System;
+using Basics.Algorithms.Sorts;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace Basics.Algorithms.Tests
+{
+    [TestClass]
+    public class ArrayExtensionsTests
+    {
+        [TestMethod]
+        public void Int_TestIsSorted()
+        {
+            var array = new int[] { 0, 1, 2, 3, 4, 5, 6, 7, 8, 9 };
+            Assert.IsTrue(array.IsSorted());
+        }
+
+        [TestMethod]
+        public void Int_TestIsNotSorted()
+        {
+            var array = new int[] { 0, 3, 9, 1, 4, 5, 2, 7, 8, 6 };
+            Assert.IsFalse(array.IsSorted());
+        }
+
+        [TestMethod]
+        public void Double_TestIsSorted()
+        {
+            var array = new double[] { 0, 1.0, 2.1, 3.3, 4, 5, 6, 7, 8, 9 };
+            Assert.IsTrue(array.IsSorted());
+        }
+
+        [TestMethod]
+        public void Double_TestIsNotSorted()
+        {
+            var array = new double[] { 0, 3, 9.1, 1, 4.5, 5, 2.8, 7, 8, 6 };
+            Assert.IsFalse(array.IsSorted());
+        }
+
+        [TestMethod]
+        public void String_TestIsSorted()
+        {
+            var array = new string[] { "a", "b", "c", "d", "e", "f", "g" };
+            Assert.IsTrue(array.IsSorted());
+        }
+
+        [TestMethod]
+        public void String_TestIsNotSorted()
+        {
+            var array = new string[] { "f", "b", "g", "d", "e", "a", "c" };
+            Assert.IsFalse(array.IsSorted());
+        }
+
+        private class GenericComparableData : IComparable<GenericComparableData>
+        {
+            private int _value;
+
+            public GenericComparableData(int value)
+            {
+                _value = value;
+            }
+
+            public int CompareTo(GenericComparableData other)
+            {
+                return _value.CompareTo(other._value);
+            }
+        }
+
+        [TestMethod]
+        public void GenericComparable_TestIsSorted()
+        {
+            var array = new GenericComparableData[]
+            {
+                new GenericComparableData(0),
+                new GenericComparableData(1),
+                new GenericComparableData(2),
+                new GenericComparableData(3)
+            };
+            Assert.IsTrue(array.IsSorted());
+        }
+
+        [TestMethod]
+        public void GenericComparable_TestIsNotSorted()
+        {
+            var array = new GenericComparableData[]
+            {
+                new GenericComparableData(0),
+                new GenericComparableData(3),
+                new GenericComparableData(2),
+                new GenericComparableData(1)
+            };
+            Assert.IsFalse(array.IsSorted());
+        }
+    }
+}
---
+++ Basics.Algorithms/Sorts/ArrayExtensions.cs
@@ -0,0 +1,52 @@
+﻿using System;
+
+namespace Basics.Algorithms.Sorts
+{
+    public static class ArrayExtensions
+    {
+        public static bool IsLessThan<T>(this IComparable<T> source, T value)
+        {
+            return source.CompareTo(value) < 0;
+        }
+
+        public static bool IsGreaterThan<T>(this IComparable<T> source, T value)
+        {
+            return source.CompareTo(value) > 0;
+        }
+
+        public static bool IsLessThan(this IComparable source, object value)
+        {
+            return source.CompareTo(value) < 0;
+        }
+
+        public static bool IsGreaterThan(this IComparable source, object value)
+        {
+            return source.CompareTo(value) > 0;
+        }
+
+        public static void Exchange<T>(this T[] source, int i, int j)
+        {
+            var tmp = source[i];
+            source[i] = source[j];
+            source[j] = tmp;
+        }
+
+        /// <summary>
+        /// Checks whether array is sorted.
+        /// Empty and arrays with one element are consedered as sorted.
+        /// </summary>
+        /// <returns>True if array is sorted, false otherwise.</returns>
+        public static bool IsSorted<T>(this T[] source) where T : IComparable<T>
+        {
+            if (source.Length < 1)
+                return true;
+
+            for (int i = 1; i < source.Length; i++)
+            {
+                if (source[i].IsLessThan(source[i - 1]))
+                    return false;
+            }
+            return true;
+        }
+    }
+}
