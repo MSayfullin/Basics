ae451f27ae9b5a8d594d2d727081f3618d5ed78d
Marsel Sayfullin <M.Sayfullin@gmail.com>
2013-10-27 08:51:15 +0400

    Refactor base logic for Linked List based collections

---
+++ Basics.Structures/LinkedListBase.cs
@@ -0,0 +1,102 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+
+namespace Basics.Structures
+{
+    public abstract class LinkedListBase<T> : IEnumerable<T>
+    {
+        protected class Node<X>
+        {
+            public X Value { get; set; }
+            public Node<X> Next { get; set; }
+        }
+
+        protected int _size = 0;
+        protected Node<T> _head = null;
+
+        protected LinkedListBase()
+        {
+        }
+
+        #region IEnumerable Members
+
+        public IEnumerator<T> GetEnumerator()
+        {
+            return new LinkedListEnumerator<T>(_head);
+            //return YieldEnumerator;
+        }
+
+        IEnumerator IEnumerable.GetEnumerator()
+        {
+            return GetEnumerator();
+        }
+
+        #endregion
+
+        #region Enumerators
+
+        private IEnumerator<T> YieldEnumerator
+        {
+            get
+            {
+                var current = _head;
+                while (current != null)
+                {
+                    yield return current.Value;
+                    current = current.Next;
+                }
+            }
+        }
+
+        private struct LinkedListEnumerator<X> : IEnumerator<X>
+        {
+            private readonly Node<X> _head;
+            private Node<X> _current;
+
+            public LinkedListEnumerator(Node<X> head)
+            {
+                _head = head;
+                _current = null;
+            }
+
+            public X Current
+            {
+                get
+                {
+                    if (_current == null)
+                        throw new InvalidOperationException("Current is undefined. Call MoveNext first.");
+
+                    return _current.Value;
+                }
+            }
+
+            public bool MoveNext()
+            {
+                _current = _current == null ? _head : _current.Next;
+                return _current != null;
+            }
+
+            public void Reset()
+            {
+                _current = null;
+            }
+
+            object IEnumerator.Current
+            {
+                get { return Current; }
+            }
+
+            #region IDisposable Members
+
+            public void Dispose()
+            {
+                // nothing to dispose
+            }
+
+            #endregion
+        }
+
+        #endregion
+    }
+}
--- Basics.Structures/Stacks/StackOnList.cs
+++ Basics.Structures/Stacks/StackOnList.cs
@@ -1,20 +1,9 @@
 ﻿using System;
-using System.Collections;
-using System.Collections.Generic;
 
 namespace Basics.Structures
 {
-    public class StackOnList<T> : IStack<T>
+    public class StackOnList<T> : LinkedListBase<T>, IStack<T>
     {
-        private class Node<X>
-        {
-            public X Value { get; set; }
-            public Node<X> Next { get; set; }
-        }
-
-        private int _size = 0;
-        private Node<T> _head = null;
-
         /// <summary>
         /// Pushes value to the stack.
         /// </summary>
@@ -58,85 +47,5 @@ namespace Basics.Structures
         {
             get { return _size; }
         }
-
-        #region IEnumerable Members
-
-        public IEnumerator<T> GetEnumerator()
-        {
-            return new LinkedListEnumerator<T>(_head);
-            //return YieldEnumerator;
-        }
-
-        IEnumerator IEnumerable.GetEnumerator()
-        {
-            return GetEnumerator();
-        }
-
-        #endregion
-
-        #region Enumerators
-
-        private IEnumerator<T> YieldEnumerator
-        {
-            get
-            {
-                var current = _head;
-                while (current != null)
-                {
-                    yield return current.Value;
-                    current = current.Next;
-                }
-            }
-        }
-
-        private struct LinkedListEnumerator<X> : IEnumerator<X>
-        {
-            private readonly Node<X> _head;
-            private Node<X> _current;
-
-            public LinkedListEnumerator(Node<X> head)
-            {
-                _head = head;
-                _current = null;
-            }
-
-            public X Current
-            {
-                get
-                {
-                    if (_current == null)
-                        throw new InvalidOperationException("Current is undefined. Call MoveNext first.");
-
-                    return _current.Value;
-                }
-            }
-
-            public bool MoveNext()
-            {
-                _current = _current == null ? _head : _current.Next;
-                return _current != null;
-            }
-
-            public void Reset()
-            {
-                _current = null;
-            }
-
-            object IEnumerator.Current
-            {
-                get { return Current; }
-            }
-
-            #region IDisposable Members
-
-            public void Dispose()
-            {
-                // nothing to dispose
-            }
-
-            #endregion
-        }
-
-        #endregion
     }
 }
