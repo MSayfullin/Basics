c34a0cced19f49161208319e0596674d59f49215
Marsel Sayfullin <M.Sayfullin@gmail.com>
2013-10-27 12:55:03 +0400

    Implement Insertion Sort

---
+++ Basics.Algorithms.Tests/InsertionSortTests.cs
@@ -0,0 +1,73 @@
+﻿using Basics.Algorithms.Sorts;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace Basics.Algorithms.Tests
+{
+    [TestClass]
+    public class InertionSortTests
+    {
+        [TestMethod]
+        public void InsertionSort_Int_TestOnEmptyArray()
+        {
+            var array = new int[] { };
+            Insertion.Sort(array);
+            Assert.IsTrue(array.IsSorted());
+        }
+
+        [TestMethod]
+        public void InsertionSort_Int_TestOnArrayWithOneElement()
+        {
+            var array = new int[] { 1 };
+            Insertion.Sort(array);
+            Assert.IsTrue(array.IsSorted());
+        }
+
+        [TestMethod]
+        public void InsertionSort_Int_TestOnSortedArray()
+        {
+            var array = new int[] { 0, 1, 2, 3, 4, 5, 6, 7, 8, 9 };
+            Insertion.Sort(array);
+            Assert.IsTrue(array.IsSorted());
+        }
+
+        [TestMethod]
+        public void InsertionSort_Int_TestOnUnsortedArray()
+        {
+            var array = new int[] { 0, 3, 9, 1, 4, 5, 2, 7, 8, 6 };
+            Insertion.Sort(array);
+            Assert.IsTrue(array.IsSorted());
+        }
+
+        [TestMethod]
+        public void InsertionSort_Int_TestOnUnsortedArrayWithDuplicates()
+        {
+            var array = new int[] { 0, 3, 9, 7, 1, 4, 5, 2, 7, 8, 6, 3, 1 };
+            Insertion.Sort(array);
+            Assert.IsTrue(array.IsSorted());
+        }
+
+        [TestMethod]
+        public void InsertionSort_String_TestOnSortedArray()
+        {
+            var array = new string[] { "a", "b", "c", "d", "e", "f", "g" };
+            Insertion.Sort(array);
+            Assert.IsTrue(array.IsSorted());
+        }
+
+        [TestMethod]
+        public void InsertionSort_String_TestOnUnsortedArray()
+        {
+            var array = new string[] { "f", "b", "g", "d", "e", "a", "c" };
+            Insertion.Sort(array);
+            Assert.IsTrue(array.IsSorted());
+        }
+
+        [TestMethod]
+        public void InsertionSort_String_TestOnUnsortedArrayWithDuplicates()
+        {
+            var array = new string[] { "f", "b", "g", "a", "d", "e", "a", "c", "b" };
+            Insertion.Sort(array);
+            Assert.IsTrue(array.IsSorted());
+        }
+    }
+}
---
+++ Basics.Algorithms/Sorts/Insertion.cs
@@ -0,0 +1,39 @@
+﻿using System;
+
+namespace Basics.Algorithms.Sorts
+{
+    public static class Insertion
+    {
+        public static void Sort<T>(T[] array) where T : IComparable<T>
+        {
+            //EthalonImplementation(array);
+            ImplementationWithWhile(array);
+        }
+
+        private static void EthalonImplementation<T>(T[] array) where T : IComparable<T>
+        {
+            for (int i = 1; i < array.Length; i++)
+            {
+                for (int j = i; j > 0; j--)
+                {
+                    if (array[j].IsLessThan(array[j - 1]))
+                        array.Exchange(j, j - 1);
+                    else break;
+                }
+            }
+        }
+
+        private static void ImplementationWithWhile<T>(T[] array) where T : IComparable<T>
+        {
+            for (int i = 1; i < array.Length; i++)
+            {
+                int j = i;
+                while (j > 0 && array[j].IsLessThan(array[j - 1]))
+                {
+                    array.Exchange(j, j - 1);
+                    j--;
+                }
+            }
+        }
+    }
+}
