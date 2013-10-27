8dac227f43f67f7871b6eff817e4965209282d3f
Marsel Sayfullin <M.Sayfullin@gmail.com>
2013-10-27 12:03:37 +0400

    Implement Selection Sort

---
+++ Basics.Algorithms.Tests/SelectionSortTests.cs
@@ -0,0 +1,73 @@
+﻿using Basics.Algorithms.Sorts;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace Basics.Algorithms.Tests
+{
+    [TestClass]
+    public class SelectionSortTests
+    {
+        [TestMethod]
+        public void SelectionSort_Int_TestOnEmptyArray()
+        {
+            var array = new int[] { };
+            Selection.Sort(array);
+            Assert.IsTrue(array.IsSorted());
+        }
+
+        [TestMethod]
+        public void SelectionSort_Int_TestOnArrayWithOneElement()
+        {
+            var array = new int[] { 1 };
+            Selection.Sort(array);
+            Assert.IsTrue(array.IsSorted());
+        }
+
+        [TestMethod]
+        public void SelectionSort_Int_TestOnSortedArray()
+        {
+            var array = new int[] { 0, 1, 2, 3, 4, 5, 6, 7, 8, 9 };
+            Selection.Sort(array);
+            Assert.IsTrue(array.IsSorted());
+        }
+
+        [TestMethod]
+        public void SelectionSort_Int_TestOnUnsortedArray()
+        {
+            var array = new int[] { 0, 3, 9, 1, 4, 5, 2, 7, 8, 6 };
+            Selection.Sort(array);
+            Assert.IsTrue(array.IsSorted());
+        }
+
+        [TestMethod]
+        public void SelectionSort_Int_TestOnUnsortedArrayWithDuplicates()
+        {
+            var array = new int[] { 0, 3, 9, 7, 1, 4, 5, 2, 7, 8, 6, 3, 1 };
+            Selection.Sort(array);
+            Assert.IsTrue(array.IsSorted());
+        }
+
+        [TestMethod]
+        public void SelectionSort_String_TestOnSortedArray()
+        {
+            var array = new string[] { "a", "b", "c", "d", "e", "f", "g" };
+            Selection.Sort(array);
+            Assert.IsTrue(array.IsSorted());
+        }
+
+        [TestMethod]
+        public void SelectionSort_String_TestOnUnsortedArray()
+        {
+            var array = new string[] { "f", "b", "g", "d", "e", "a", "c" };
+            Selection.Sort(array);
+            Assert.IsTrue(array.IsSorted());
+        }
+
+        [TestMethod]
+        public void SelectionSort_String_TestOnUnsortedArrayWithDuplicates()
+        {
+            var array = new string[] { "f", "b", "g", "a", "d", "e", "a", "c", "b" };
+            Selection.Sort(array);
+            Assert.IsTrue(array.IsSorted());
+        }
+    }
+}
---
+++ Basics.Algorithms/Sorts/Selection.cs
@@ -0,0 +1,19 @@
+﻿using System;
+
+namespace Basics.Algorithms.Sorts
+{
+    public static class Selection
+    {
+        public static void Sort<T>(T[] array) where T : IComparable<T>
+        {
+            for (int i = 0; i < array.Length; i++)
+            {
+                for (int j = i + 1; j < array.Length; j++)
+                {
+                    if (array[j].IsLessThan(array[i]))
+                        array.Exchange(i, j);
+                }
+            }
+        }
+    }
+}
