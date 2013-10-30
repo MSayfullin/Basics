6fc6a93a9ebedec1e39ce47335ea2222cda87be0
Marsel Sayfullin <M.Sayfullin@gmail.com>
2013-10-30 22:49:54 +0400

    Implement Quick Select

---
+++ Basics.Algorithms.Tests/QuickSelectTests.cs
@@ -0,0 +1,16 @@
+﻿using Basics.Algorithms.Sorts;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace Basics.Algorithms.Tests
+{
+    [TestClass]
+    public class QuickSelectTests
+    {
+        [TestMethod]
+        public void QuickSelectTest()
+        {
+            var array = new int[] { 1, 6, 7, 3, 8, 0, 2, 4 };
+            Assert.AreEqual(3, Quick.Select(array, 3));
+        }
+    }
+}
--- Basics.Algorithms/Sorts/Quick.cs
+++ Basics.Algorithms/Sorts/Quick.cs
@@ -11,6 +11,25 @@ namespace Basics.Algorithms.Sorts
             Sort(array, 0, array.Length - 1);
         }
 
+        public static T Select<T>(T[] array, int k) where T : IComparable<T>
+        {
+            array.Shuffle();
+
+            int lo = 0;
+            int hi = array.Length - 1;
+            while (lo <= hi)
+            {
+                int j = Partition(array, lo, hi);
+                if (j < k)
+                    lo = j + 1;
+                else if (j > k)
+                    hi = j - 1;
+                else
+                    return array[k];
+            }
+            return array[k];
+        }
+
         private static void Sort<T>(T[] array, int lo, int hi) where T : IComparable<T>
         {
             if (hi <= lo) return;
