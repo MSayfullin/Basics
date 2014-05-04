30e78dd6ac730986ae3a7c21456fb479d845b07e
Marsel Sayfullin <M.Sayfullin@gmail.com>
2014-05-04 21:49:48 +0400

    Implement inversion counter

---
+++ Basics.Algorithms.Tests/InversionCountTests.cs
@@ -0,0 +1,176 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace Basics.Algorithms.Tests
+{
+    [TestClass]
+    public class InversionCountTests
+    {
+        private const string DataFilePath = @"Data\IntegerArray.txt";
+
+        private static List<int> _integers = new List<int>();
+
+        [ClassInitialize]
+        [DeploymentItem(DataFilePath)]
+        public static void LoadTestData(TestContext context)
+        {
+            using (var file = File.OpenText(DataFilePath))
+            {
+                while (!file.EndOfStream)
+	            {
+                    int val;
+                    var str = file.ReadLine();
+                    if (!str.StartsWith("#") && Int32.TryParse(str, out val))
+                    {
+                        _integers.Add(val);
+                    }
+                }
+                Assert.AreEqual(100000, _integers.Count);
+            }
+        }
+
+        [TestMethod]
+        public void InversionCount_BruteForce_Int_TestOnEmptyArray()
+        {
+            var array = new int[] { };
+            var count = Inversion.CountBruteForce(array);
+            Assert.AreEqual(0, count);
+        }
+
+        [TestMethod]
+        public void InversionCount_BruteForce_Int_TestOnArrayWithOneElement()
+        {
+            var array = new int[] { 1 };
+            var count = Inversion.CountBruteForce(array);
+            Assert.AreEqual(0, count);
+        }
+
+        [TestMethod]
+        public void InversionCount_BruteForce_Int_TestOnSortedArray()
+        {
+            var array = new int[] { 0, 1, 2, 3, 4, 5, 6, 7, 8, 9 };
+            var count = Inversion.CountBruteForce(array);
+            Assert.AreEqual(0, count);
+        }
+
+        [TestMethod]
+        public void InversionCount_BruteForce_Int_TestOnReverseSortedArray()
+        {
+            var array = new int[] { 9, 8, 7, 6, 5, 4, 3, 2, 1, 0 };
+            var count = Inversion.CountBruteForce(array);
+            Assert.AreEqual(45, count);
+        }
+
+        [TestMethod]
+        public void InversionCount_BruteForce_Int_TestOnUnsortedArray()
+        {
+            var array = new int[] { 0, 3, 9, 1, 4, 5, 2, 7, 8, 6 };
+            var count = Inversion.CountBruteForce(array);
+            Assert.AreEqual(13, count);
+        }
+
+        [TestMethod]
+        public void InversionCount_BruteForce_Int_TestOnUnsortedArrayWithDuplicates()
+        {
+            var array = new int[] { 0, 3, 9, 7, 1, 4, 5, 2, 7, 8, 6, 3, 1 };
+            var count = Inversion.CountBruteForce(array);
+            Assert.AreEqual(36, count);
+        }
+
+        [TestMethod]
+        public void InversionCount_Int_TestOnEmptyArray()
+        {
+            var array = new int[] { };
+            var ethalon = Inversion.CountBruteForce(array);
+            var count = Inversion.Count(array);
+            Assert.AreEqual(ethalon, count);
+        }
+
+        [TestMethod]
+        public void InversionCount_Int_TestOnArrayWithOneElement()
+        {
+            var array = new int[] { 1 };
+            var ethalon = Inversion.CountBruteForce(array);
+            var count = Inversion.Count(array);
+            Assert.AreEqual(ethalon, count);
+        }
+
+        [TestMethod]
+        public void InversionCount_Int_TestOnSortedArray()
+        {
+            var array = new int[] { 0, 1, 2, 3, 4, 5, 6, 7, 8, 9 };
+            var ethalon = Inversion.CountBruteForce(array);
+            var count = Inversion.Count(array);
+            Assert.AreEqual(ethalon, count);
+        }
+
+        [TestMethod]
+        public void InversionCount_Int_TestOnReverseSortedArray()
+        {
+            // inversion count = 45
+            // formula: max inversion count = n(n-1)/2
+            var array = new int[] { 9, 8, 7, 6, 5, 4, 3, 2, 1, 0 };
+            var ethalon = Inversion.CountBruteForce(array);
+            var count = Inversion.Count(array);
+            Assert.AreEqual(ethalon, count);
+        }
+
+        [TestMethod]
+        public void InversionCount_Int_TestOnUnsortedArray()
+        {
+            // inversion count = 13
+            var array = new int[] { 0, 3, 9, 1, 4, 5, 2, 7, 8, 6 };
+            var ethalon = Inversion.CountBruteForce(array);
+            var count = Inversion.Count(array);
+            Assert.AreEqual(ethalon, count);
+        }
+
+        [TestMethod]
+        public void InversionCount_Int_TestOnUnsortedArrayWithDuplicates()
+        {
+            // inversion count = 36
+            var array = new int[] { 0, 3, 9, 7, 1, 4, 5, 2, 7, 8, 6, 3, 1 };
+            var ethalon = Inversion.CountBruteForce(array);
+            var count = Inversion.Count(array);
+            Assert.AreEqual(ethalon, count);
+        }
+
+        [TestMethod]
+        public void InversionCount_String_TestOnSortedArray()
+        {
+            var array = new string[] { "a", "b", "c", "d", "e", "f", "g" };
+            var ethalon = Inversion.CountBruteForce(array);
+            var count = Inversion.Count(array);
+            Assert.AreEqual(ethalon, count);
+        }
+
+        [TestMethod]
+        public void InversionCount_String_TestOnUnsortedArray()
+        {
+            // inversion count = 14
+            var array = new string[] { "f", "b", "g", "d", "e", "a", "c" };
+            var ethalon = Inversion.CountBruteForce(array);
+            var count = Inversion.Count(array);
+            Assert.AreEqual(ethalon, count);
+        }
+
+        [TestMethod]
+        public void InversionCount_String_TestOnUnsortedArrayWithDuplicates()
+        {
+            // inversion count = 22
+            var array = new string[] { "f", "b", "g", "a", "d", "e", "a", "c", "b" };
+            var ethalon = Inversion.CountBruteForce(array);
+            var count = Inversion.Count(array);
+            Assert.AreEqual(ethalon, count);
+        }
+
+        [TestMethod]
+        public void InversionCount_Int_TestOnLargeData()
+        {
+            var count = Inversion.Count(_integers.ToArray());
+            Assert.AreEqual(2407905288, count);
+        }
+    }
+}
---
+++ Basics.Algorithms/Inversion.cs
@@ -0,0 +1,71 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace Basics.Algorithms
+{
+    public static class Inversion
+    {
+        /// <summary>
+        /// For testing purposes only.
+        /// </summary>
+        internal static long CountBruteForce<T>(T[] array) where T : IComparable<T>, IComparable
+        {
+            long counter = 0;
+            for (int i = 0; i < array.Length; i++)
+            {
+                for (int j = i + 1; j < array.Length; j++)
+                {
+                    if (array[i].IsGreaterThan(array[j])) counter++;
+                }
+            }
+            return counter;
+        }
+
+        public static long Count<T>(T[] array) where T : IComparable<T>, IComparable
+        {
+            var aux = new T[array.Length];
+            var copy = new T[array.Length];
+            array.CopyTo(copy, 0);
+
+            return Count<T>(copy, aux, 0, array.Length - 1);
+        }
+
+        private static long Count<T>(T[] array, T[] aux, int lo, int hi) where T : IComparable<T>, IComparable
+        {
+            if (lo >= hi) return 0;
+
+            var mid = (hi - lo) / 2 + lo;
+            var x = Count(array, aux, lo, mid);
+            var y = Count(array, aux, mid + 1, hi);
+            var z = MergeAndCount(array, aux, lo, mid, hi);
+            return x + y + z;
+        }
+
+        private static long MergeAndCount<T>(T[] array, T[] aux, int lo, int mid, int hi) where T : IComparable<T>, IComparable
+        {
+            for (int k = lo; k <= hi; k++)
+            {
+                aux[k] = array[k];
+            }
+
+            int i = lo;
+            int j = mid + 1;
+            long counter = 0;
+            for (int k = lo; k <= hi; k++)
+            {
+                if (i > mid)
+                    array[k] = aux[j++];
+                else if (j > hi)
+                    array[k] = aux[i++];
+                else if (aux[i].CompareTo(aux[j]) <= 0) // is less than or equal
+                    array[k] = aux[i++];
+                else
+                {
+                    array[k] = aux[j++];
+                    counter += mid - i + 1;
+                }
+            }
+            return counter;
+        }
+    }
+}
