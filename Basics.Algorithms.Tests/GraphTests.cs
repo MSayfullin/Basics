20abdebb58634b9b36ae5df44b2ec3d05b3d7c30
Marsel Sayfullin <M.Sayfullin@gmail.com>
2014-06-03 22:31:44 +0400

    Implement Randomized MinCut
    Random Contraction Algorithm by Karger

---
+++ Basics.Algorithms.Tests/GraphTests.cs
@@ -0,0 +1,61 @@
+﻿using System;
+using System.IO;
+using System.Linq;
+using Basics.Algorithms.Graphs;
+using Basics.Structures.Graphs;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace Basics.Algorithms.Tests
+{
+    [TestClass]
+    public class GraphTests
+    {
+        [TestMethod]
+        public void MinCut_SmallGraphTest()
+        {
+            var graph = new AdjacencyListGraph<int>();
+            graph.AddEdge(1, 2);
+            graph.AddEdge(1, 3);
+            graph.AddEdge(1, 4);
+            graph.AddEdge(2, 3);
+            graph.AddEdge(3, 4);
+
+            var minCut = graph.RandomizedMinCut();
+            Assert.AreEqual(2, minCut.Count());
+        }
+
+        [TestMethod]
+        [DeploymentItem(@"Data\UndirectedGraph.txt")]
+        public void MinCut_LargeGraphTest()
+        {
+            var graph = LoadGraph();
+            var minCut = graph.RandomizedMinCut();
+            // actual MinCut is 17 because graph is undirected
+            Assert.AreEqual(34, minCut.Count());
+        }
+
+        private static IGraph<string> LoadGraph()
+        {
+            var graph = new AdjacencyListGraph<string>();
+            using (var file = File.OpenText("UndirectedGraph.txt"))
+            {
+                while (!file.EndOfStream)
+                {
+                    var str = file.ReadLine();
+                    if (!str.StartsWith("#"))
+                    {
+                        var data = str.Split(new[] { '\t' }, StringSplitOptions.RemoveEmptyEntries);
+                        var source = data[0];
+                        for (int i = 1; i < data.Length; i++)
+                        {
+                            var target = data[i];
+                            graph.AddEdge(source, target);
+                        }
+                    }
+                }
+                Assert.AreEqual(200, graph.VertexCount);
+            }
+            return graph;
+        }
+    }
+}
---
+++ Basics.Algorithms/Graphs/MinCut.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Collections.Generic;
+using Basics.Structures.Graphs;
+
+namespace Basics.Algorithms.Graphs
+{
+    public static class MinCut
+    {
+        public static IEnumerable<Edge<T>> RandomizedMinCut<T>(this IGraph<T> graph) where T : IEquatable<T>
+        {
+            var rnd = new Random();
+            List<Edge<T>> minCut = null;
+            for (int i = 0; i < graph.VertexCount; i++)
+            {
+                var edges = CollapseEdges(rnd, graph.VertexCount, graph.GetEdges());
+                if (minCut == null || edges.Count < minCut.Count)
+                {
+                    minCut = edges;
+                }
+            }
+            return minCut;
+        }
+
+        private static List<Edge<T>> CollapseEdges<T>(Random rnd, int nodeCount, IEnumerable<Edge<T>> edges) where T : IEquatable<T>
+        {
+            var edgesList = new List<Edge<T>>(edges);
+            while (nodeCount > 2)
+            {
+                nodeCount--;
+                var idx = rnd.Next(edgesList.Count);
+                var current = edgesList[idx];
+                edgesList.RemoveAt(idx);
+
+                var copy = new List<Edge<T>>();
+                foreach (var edge in edgesList)
+                {
+                    var correctedEdge =
+                        edge.Source.Equals(current.Target) ? new Edge<T>(current.Source, edge.Target)
+                        : edge.Target.Equals(current.Target) ? new Edge<T>(edge.Source, current.Source)
+                        : edge;
+
+                    if (!correctedEdge.IsSelfLooped)
+                    {
+                        copy.Add(correctedEdge);
+                    }
+                }
+                edgesList = copy;
+            }
+            return edgesList;
+        }
+    }
+}
