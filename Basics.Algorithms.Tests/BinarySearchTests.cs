dc55f222813aff1d637628955d71aca539ed02f3
Marsel Sayfullin <M.Sayfullin@gmail.com>
2014-06-03 22:31:54 +0400

    Fix typo

--- Basics.Algorithms.Tests/BinarySearchTests.cs
+++ Basics.Algorithms.Tests/BinarySearchTests.cs
@@ -40,7 +40,7 @@ namespace Basics.Algorithms.Tests
         }
 
         [TestMethod]
-        public void BinarySearch_MidddleKey()
+        public void BinarySearch_MiddleKey()
         {
             var array = new int[] { 0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10 };
             var index = array.IndexOf(5);
